30a40e724d70001b7a7ae71474b8c9d5ac95d74d
Pi1lot <poellen.dorian@gmail.com>
2025-11-17 15:38:40 +0100

    Suite TD3

--- td3/exercice_1/main.cpp
+++ td3/exercice_1/main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <map>
 
 using namespace std;
 
 void countFrequencyBruteForce(const vector<int> &numbers)
 {
-    for (int i = 0; i < numbers.size(); ++i)
+    vector<int> sorted;
+    sorted = numbers;
+    sort(sorted.begin(), sorted.end());
+    cout << "Seconde liste triée: ";
+    for (int value : sorted){
+        cout << value << " ";
+    }
+
+    cout << "Itération" << endl;
+
+    int currentIndex = 0;
+    //= sort(numbers.begin(), numbers.end());
+    for (int i = 0; i < sorted.size(); ++i)
     {
         int occurence = 0;
-        for (int j = 0; j < numbers.size(); ++j)
+
+        for (int j = 0; j < sorted.size(); ++j)
         {
-            if (numbers[i] == numbers[j])
+            if (sorted[i] == sorted[j])
             {
                 occurence++;
             }
         }
-        cout << numbers[i] << ": " << occurence << " times" << endl;
+
+        // On affiche uniquement si on n'a pas déjà affiché cet élément
+        if (currentIndex != sorted[i]) {
+            cout << sorted[i] << ": " << occurence << " times" << endl;
+            currentIndex = sorted[i];
+        }
     }
 }
 
@@ -32,7 +51,7 @@ map<int, int> countFrequencyOptimal(const vector<int> &numbers)
 
 int main()
 {
-vector<int> numbers = {1, 2, 3, 2, 4, 1, 5, 5, 6};
+vector<int> numbers = {1, 1, 1, 1,  2, 3, 2, 4, 6, 1, 5, 5, 6};
 
 // Test countFrequencyBruteForce
 cout << "Frequency (Brute Force):" << endl;
